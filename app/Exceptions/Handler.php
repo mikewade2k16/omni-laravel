d6c81d71db3fab0df24917e3b4398e65f6307a0a
victor18edu <victor.18edu@gmail.com>
2025-09-16 16:03:12 -0300

    autenticacao_adicionada

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Auth\AuthenticationException;
 
 class Handler extends ExceptionHandler
 {
@@ -25,6 +26,14 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+        
+        $this->renderable(function (AuthenticationException $e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Token inválido ou expirado.'
+                ], 401);
+            }
         });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\TaskController;
 use App\Http\Controllers\Admin\ClientController;
@@ -29,122 +30,128 @@ use App\Http\Controllers\Admin\ColumnHistoryController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register']);
 
 
+Route::group(['middleware' => 'auth:api'], function () {
 
-Route::prefix('admin')->group(function () {
-    // Rotas para Users
-    Route::get('users', [UserController::class, 'index']);
-    Route::post('users', [UserController::class, 'store']);
-    Route::get('users/{id}', [UserController::class, 'show']);
-    Route::put('users/{id}', [UserController::class, 'update']);
-    Route::delete('users/{id}', [UserController::class, 'delete']);
+    // Rotas relacionadas à gestão da autenticação do próprio usuário
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/me', [AuthController::class, 'me']);
 
-    // Rotas para Tasks
-    Route::get('tasks', [TaskController::class, 'index']);
-    Route::post('tasks', [TaskController::class, 'store']);
-    Route::get('tasks/{id}', [TaskController::class, 'show']);
-    Route::put('tasks/{id}', [TaskController::class, 'update']);
-    Route::delete('tasks/{id}', [TaskController::class, 'delete']);
+    Route::prefix('admin')->group(function () {
+        // Rotas para Users
+        Route::get('users', [UserController::class, 'index']);
+        Route::post('users', [UserController::class, 'store']);
+        Route::get('users/{id}', [UserController::class, 'show']);
+        Route::put('users/{id}', [UserController::class, 'update']);
+        Route::delete('users/{id}', [UserController::class, 'delete']);
 
-    // Rotas para Clients
-    Route::get('clients', [ClientController::class, 'index']);
-    Route::post('clients', [ClientController::class, 'store']);
-    Route::get('clients/{id}', [ClientController::class, 'show']);
-    Route::put('clients/{id}', [ClientController::class, 'update']);
-    Route::delete('clients/{id}', [ClientController::class, 'delete']);
+        // Rotas para Tasks
+        Route::get('tasks', [TaskController::class, 'index']);
+        Route::post('tasks', [TaskController::class, 'store']);
+        Route::get('tasks/{id}', [TaskController::class, 'show']);
+        Route::put('tasks/{id}', [TaskController::class, 'update']);
+        Route::delete('tasks/{id}', [TaskController::class, 'delete']);
 
-    // Rotas para Campaigns
-    Route::get('campaigns', [CampaignController::class, 'index']);
-    Route::post('campaigns', [CampaignController::class, 'store']);
-    Route::get('campaigns/{id}', [CampaignController::class, 'show']);
-    Route::put('campaigns/{id}', [CampaignController::class, 'update']);
-    Route::delete('campaigns/{id}', [CampaignController::class, 'delete']);
-    
-    // Rotas para Trackings
-    Route::get('trackings', [TrackingController::class, 'index']);
-    Route::post('trackings', [TrackingController::class, 'store']);
-    Route::get('trackings/{id}', [TrackingController::class, 'show']);
-    Route::put('trackings/{id}', [TrackingController::class, 'update']);
-    Route::delete('trackings/{id}', [TrackingController::class, 'delete']);
+        // Rotas para Clients
+        Route::get('clients', [ClientController::class, 'index']);
+        Route::post('clients', [ClientController::class, 'store']);
+        Route::get('clients/{id}', [ClientController::class, 'show']);
+        Route::put('clients/{id}', [ClientController::class, 'update']);
+        Route::delete('clients/{id}', [ClientController::class, 'delete']);
 
-    // Rotas para Collections
-    Route::get('collections', [CollectionController::class, 'index']);
-    Route::post('collections', [CollectionController::class, 'store']);
-    Route::get('collections/{id}', [CollectionController::class, 'show']);
-    Route::put('collections/{id}', [CollectionController::class, 'update']);
-    Route::delete('collections/{id}', [CollectionController::class, 'delete']);
+        // Rotas para Campaigns
+        Route::get('campaigns', [CampaignController::class, 'index']);
+        Route::post('campaigns', [CampaignController::class, 'store']);
+        Route::get('campaigns/{id}', [CampaignController::class, 'show']);
+        Route::put('campaigns/{id}', [CampaignController::class, 'update']);
+        Route::delete('campaigns/{id}', [CampaignController::class, 'delete']);
+        
+        // Rotas para Trackings
+        Route::get('trackings', [TrackingController::class, 'index']);
+        Route::post('trackings', [TrackingController::class, 'store']);
+        Route::get('trackings/{id}', [TrackingController::class, 'show']);
+        Route::put('trackings/{id}', [TrackingController::class, 'update']);
+        Route::delete('trackings/{id}', [TrackingController::class, 'delete']);
 
-    // Rotas para Collection Items
-    Route::get('collection-items', [CollectionItemController::class, 'index']);
-    Route::post('collection-items', [CollectionItemController::class, 'store']);
-    Route::get('collection-items/{id}', [CollectionItemController::class, 'show']);
-    Route::put('collection-items/{id}', [CollectionItemController::class, 'update']);
-    Route::delete('collection-items/{id}', [CollectionItemController::class, 'delete']);
+        // Rotas para Collections
+        Route::get('collections', [CollectionController::class, 'index']);
+        Route::post('collections', [CollectionController::class, 'store']);
+        Route::get('collections/{id}', [CollectionController::class, 'show']);
+        Route::put('collections/{id}', [CollectionController::class, 'update']);
+        Route::delete('collections/{id}', [CollectionController::class, 'delete']);
 
-    // Rotas para Collection Views
-    Route::get('collection-views', [CollectionViewController::class, 'index']);
-    Route::post('collection-views', [CollectionViewController::class, 'store']);
-    Route::get('collection-views/{id}', [CollectionViewController::class, 'show']);
-    Route::put('collection-views/{id}', [CollectionViewController::class, 'update']);
-    Route::delete('collection-views/{id}', [CollectionViewController::class, 'delete']);
+        // Rotas para Collection Items
+        Route::get('collection-items', [CollectionItemController::class, 'index']);
+        Route::post('collection-items', [CollectionItemController::class, 'store']);
+        Route::get('collection-items/{id}', [CollectionItemController::class, 'show']);
+        Route::put('collection-items/{id}', [CollectionItemController::class, 'update']);
+        Route::delete('collection-items/{id}', [CollectionItemController::class, 'delete']);
 
-    // Rotas para FilesOmnis
-    Route::get('files-omnis', [FilesOmniController::class, 'index']);
-    Route::post('files-omnis', [FilesOmniController::class, 'store']);
-    Route::get('files-omnis/{id}', [FilesOmniController::class, 'show']);
-    Route::put('files-omnis/{id}', [FilesOmniController::class, 'update']);
-    Route::delete('files-omnis/{id}', [FilesOmniController::class, 'delete']);
+        // Rotas para Collection Views
+        Route::get('collection-views', [CollectionViewController::class, 'index']);
+        Route::post('collection-views', [CollectionViewController::class, 'store']);
+        Route::get('collection-views/{id}', [CollectionViewController::class, 'show']);
+        Route::put('collection-views/{id}', [CollectionViewController::class, 'update']);
+        Route::delete('collection-views/{id}', [CollectionViewController::class, 'delete']);
 
-    // Rotas para Projects
-    Route::get('projects', [ProjectController::class, 'index']);
-    Route::post('projects', [ProjectController::class, 'store']);
-    Route::get('projects/{id}', [ProjectController::class, 'show']);
-    Route::put('projects/{id}', [ProjectController::class, 'update']);
-    Route::delete('projects/{id}', [ProjectController::class, 'delete']);
+        // Rotas para FilesOmnis
+        Route::get('files-omnis', [FilesOmniController::class, 'index']);
+        Route::post('files-omnis', [FilesOmniController::class, 'store']);
+        Route::get('files-omnis/{id}', [FilesOmniController::class, 'show']);
+        Route::put('files-omnis/{id}', [FilesOmniController::class, 'update']);
+        Route::delete('files-omnis/{id}', [FilesOmniController::class, 'delete']);
 
-    // Rotas para UserProjects
-    Route::get('user-projects', [UserProjectController::class, 'index']);
-    Route::post('user-projects', [UserProjectController::class, 'store']);
-    Route::get('user-projects/{id}', [UserProjectController::class, 'show']);
-    Route::put('user-projects/{id}', [UserProjectController::class, 'update']);
-    Route::delete('user-projects/{id}', [UserProjectController::class, 'delete']);
+        // Rotas para Projects
+        Route::get('projects', [ProjectController::class, 'index']);
+        Route::post('projects', [ProjectController::class, 'store']);
+        Route::get('projects/{id}', [ProjectController::class, 'show']);
+        Route::put('projects/{id}', [ProjectController::class, 'update']);
+        Route::delete('projects/{id}', [ProjectController::class, 'delete']);
 
-    // Rotas para Qrcodes
-    Route::get('qrcodes', [QrcodeController::class, 'index']);
-    Route::post('qrcodes', [QrcodeController::class, 'store']);
-    Route::get('qrcodes/{id}', [QrcodeController::class, 'show']);
-    Route::put('qrcodes/{id}', [QrcodeController::class, 'update']);
-    Route::delete('qrcodes/{id}', [QrcodeController::class, 'delete']);
+        // Rotas para UserProjects
+        Route::get('user-projects', [UserProjectController::class, 'index']);
+        Route::post('user-projects', [UserProjectController::class, 'store']);
+        Route::get('user-projects/{id}', [UserProjectController::class, 'show']);
+        Route::put('user-projects/{id}', [UserProjectController::class, 'update']);
+        Route::delete('user-projects/{id}', [UserProjectController::class, 'delete']);
 
-    // Rotas para ShortLinks
-    Route::get('short-links', [ShortLinkController::class, 'index']);
-    Route::post('short-links', [ShortLinkController::class, 'store']);
-    Route::get('short-links/{id}', [ShortLinkController::class, 'show']);
-    Route::put('short-links/{id}', [ShortLinkController::class, 'update']);
-    Route::delete('short-links/{id}', [ShortLinkController::class, 'delete']);
+        // Rotas para Qrcodes
+        Route::get('qrcodes', [QrcodeController::class, 'index']);
+        Route::post('qrcodes', [QrcodeController::class, 'store']);
+        Route::get('qrcodes/{id}', [QrcodeController::class, 'show']);
+        Route::put('qrcodes/{id}', [QrcodeController::class, 'update']);
+        Route::delete('qrcodes/{id}', [QrcodeController::class, 'delete']);
 
-    // Rotas para SiteZen
-    Route::get('site-zen', [SiteZenController::class, 'index']);
-    Route::post('site-zen', [SiteZenController::class, 'store']);
-    Route::get('site-zen/{id}', [SiteZenController::class, 'show']);
-    Route::put('site-zen/{id}', [SiteZenController::class, 'update']);
-    Route::delete('site-zen/{id}', [SiteZenController::class, 'delete']);
+        // Rotas para ShortLinks
+        Route::get('short-links', [ShortLinkController::class, 'index']);
+        Route::post('short-links', [ShortLinkController::class, 'store']);
+        Route::get('short-links/{id}', [ShortLinkController::class, 'show']);
+        Route::put('short-links/{id}', [ShortLinkController::class, 'update']);
+        Route::delete('short-links/{id}', [ShortLinkController::class, 'delete']);
 
-    // Rotas para Columns
-    Route::get('columns', [ColumnController::class, 'index']);
-    Route::post('columns', [ColumnController::class, 'store']);
-    Route::get('columns/{id}', [ColumnController::class, 'show']);
-    Route::put('columns/{id}', [ColumnController::class, 'update']);
-    Route::delete('columns/{id}', [ColumnController::class, 'delete']);
+        // Rotas para SiteZen
+        Route::get('site-zen', [SiteZenController::class, 'index']);
+        Route::post('site-zen', [SiteZenController::class, 'store']);
+        Route::get('site-zen/{id}', [SiteZenController::class, 'show']);
+        Route::put('site-zen/{id}', [SiteZenController::class, 'update']);
+        Route::delete('site-zen/{id}', [SiteZenController::class, 'delete']);
 
-    // Rotas para Column Histories
-    Route::get('column-histories', [ColumnHistoryController::class, 'index']);
-    Route::post('column-histories', [ColumnHistoryController::class, 'store']);
-    Route::get('column-histories/{id}', [ColumnHistoryController::class, 'show']);
-    Route::put('column-histories/{id}', [ColumnHistoryController::class, 'update']);
-    Route::delete('column-histories/{id}', [ColumnHistoryController::class, 'delete']);        
+        // Rotas para Columns
+        Route::get('columns', [ColumnController::class, 'index']);
+        Route::post('columns', [ColumnController::class, 'store']);
+        Route::get('columns/{id}', [ColumnController::class, 'show']);
+        Route::put('columns/{id}', [ColumnController::class, 'update']);
+        Route::delete('columns/{id}', [ColumnController::class, 'delete']);
+
+        // Rotas para Column Histories
+        Route::get('column-histories', [ColumnHistoryController::class, 'index']);
+        Route::post('column-histories', [ColumnHistoryController::class, 'store']);
+        Route::get('column-histories/{id}', [ColumnHistoryController::class, 'show']);
+        Route::put('column-histories/{id}', [ColumnHistoryController::class, 'update']);
+        Route::delete('column-histories/{id}', [ColumnHistoryController::class, 'delete']);        
+    });
 });
